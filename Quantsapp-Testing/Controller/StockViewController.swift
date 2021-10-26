6a7ae04dd55186143a19ffa3066e67c9609f3396
Anis <anishagwan123@gmail.com>
2021-10-27 01:21:56 +0530

    splitting the strings into respective stocks

--- Quantsapp-Testing/Controller/StockViewController.swift
+++ Quantsapp-Testing/Controller/StockViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 class StockViewController: UIViewController {
 
     var stockManager = StockManager()
+    var allStockPrice: [Stock]?
+    var longStockPrice: [Stock]?
+    var shortStockPrice: [Stock]?
+    var luStockPrice: [Stock]?
+    var scStockPrice: [Stock]?
+    
+//    @IBOutlet weak var stockCollectionView: UICollectionView!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         stockManager.delegate = self
+        searchTextField.delegate = self
         
         stockManager.performRequest(with: "https://qapptemporary.s3.ap-south-1.amazonaws.com/test/synopsis.json")
     }
@@ -23,10 +32,18 @@ class StockViewController: UIViewController {
 
 extension StockViewController: StockmanagerDelegate {
     func didUpdateStocks(_ stockManager: StockManager, stock: StockModel) {
-        print("STOCKS L: \(stock.l)")
-        print("STOCKS: \(stock.lu)")
-        print("STOCKS: \(stock.s)")
-        print("STOCKS: \(stock.sc)")
+        //print("STOCKS L: \(stock.l)")
+        
+        //print("SPLIT: \(stock.l.split(separator: ";"))")
+        
+        let allStr = stock.l + stock.lu + stock.s + stock.sc
+        
+        self.allStockPrice = stringSplitArr(allStr)
+        
+        self.longStockPrice = stringSplitArr(stock.l)
+        self.luStockPrice = stringSplitArr(stock.lu)
+        self.shortStockPrice = stringSplitArr(stock.s)
+        self.scStockPrice = stringSplitArr(stock.sc)
     }
     
     func didFailWithError(error: Error) {
@@ -34,4 +51,35 @@ extension StockViewController: StockmanagerDelegate {
     }
     
     
+    func stringSplitArr(_ str: String) -> [Stock] {
+        let tempArr = str.split(separator: ";")
+        var tempStockArr = [Stock]()
+        
+        tempArr.forEach { stc in
+            //print("STC: \(stc)")
+            let stcTempArr = stc.split(separator: ",")
+            let stck = Stock(symbol: String(stcTempArr[0]), price: String(stcTempArr[1]), openInterest: String(stcTempArr[2]), priceChangePercent: String(stcTempArr[3]), openInterestChangePercent: String(stcTempArr[4]))
+            //print("STOCK: \(stck)")
+            tempStockArr.append(stck)
+            }
+        
+        return tempStockArr
+        
+    }
+    
 }
+
+extension StockViewController: UITextFieldDelegate {
+    
+}
+
+//extension StockViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 0
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        //return cell
+//    }
+//
+//}
---
+++ Quantsapp-Testing/Model/Stock.swift
@@ -0,0 +1,16 @@
+//
+//  Stock.swift
+//  Quantsapp-Testing
+//
+//  Created by Anis Agwan on 27/10/21.
+//
+
+import Foundation
+
+struct Stock {
+    let symbol: String
+    let price: String
+    let openInterest: String
+    let priceChangePercent: String
+    let openInterestChangePercent: String
+}
